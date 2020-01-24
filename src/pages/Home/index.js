8d1e8bf624eceb326060b9e545255e688fe083b2
Eliton-Dioni <eliton@sou.faccat.br>
2020-01-24 16:51:58 -0300

    frontend

---
+++ src/pages/Home/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import image from '../../assets/image.jpg';
+
+export default function Home() {
+    return (
+        <div className="container">
+            <h1>Você está <strong>Logado</strong></h1>
+            <img src={image} alt="Dogmeme" id="dog"/>
+        </div>
+    );
+}
